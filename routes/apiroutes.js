78398fb1422a2bc0d3dd6f48ee5dce63341580b4
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-10-07 16:55:34 -0400

    trying to sort out api

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,5 +1,17 @@
-// const { Router } = require("express")
 
+const express = require("express");
+const app = express();
+// const store = require('../db/store');
+const notes = require('../db/db.json');
 // get Route
+module.exports = function(app) {
+app.get("/api/notes", function(req, res) {
+    res.json(notes);
+});
 // post route 
+// app.post("/api/notes", function(req, res) {
+//     notes.push(req.body);
+//     res.send('')
+// });
+};
 // delete
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"))
     })
+
     app.get("/notes", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"))
     })
