29734107096b318e0aaf43041236dde7b1bf37fd
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-10-08 18:19:36 -0400

    getting my uuid correct, removed ffrom store

--- db/store.js
+++ db/store.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
+const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 const { stringify } = require("querystring");
-const util = require("util");
 // class for notes
 class Store {
 
