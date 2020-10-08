067caac9dc1ed1a6b478ac5b2b6774614e3c61e6
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-10-08 15:55:19 -0400

    getting help with store.js

--- db/store.js
+++ db/store.js
@@ -1,20 +1,19 @@
-class Notes {
-    constructor(title, body) {
-        this.tytle = title;
-        this.body = body;
+const fs = require("fs");
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+class Store {
+
+    // // defined functions that read, write, and delete
+    read() {
+        return readFileAsync("db/db.json", "utf8");
+    }
+
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note));
+    }
+
+    del() {
+
     }
 }
-// defined fucntions that read, write, and delete
-function read() {
-
-}
-
-function write() {
-
-}
-
-function del() {
-
-}
-
-module.exports = ;
+module.exports = new Store();
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,16 +2,23 @@
 const express = require("express");
 const app = express();
 // const store = require('../db/store');
-const notes = require('../db/store');
+const store = require('../db/store');
 // get Route
-module.exports = function(app) {
-app.get("/api/notes", function(req, res) {
-    res.json(notes);
-});
-// post route 
-// app.post("/api/notes", function(req, res) {
-//     notes.push(req.body);
-//     res.send('')
-// });
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        store.read().then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err))
+    });
+    // post route 
+    app.post("/api/notes", function(req, res) {
+        // passed the data from the request to the class method
+        store.write(req.body).then(note => res.json(note))
+        .catch(err => res.status(500).json(err))
+    });
+    // delete route
+    app.delete("/api/notes/:id", function(req, res) {
+        store.delete(req.params.id).then(() => res.json())
+        .catch(err => res.status(500).json(err))
+    })
 };
-// delete
+
